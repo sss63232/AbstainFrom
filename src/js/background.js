013380f467f82e0706ab968229f278045e0748e6
YU-HSIN, CHEN <s63232@gmail.com>
2018-05-22 19:39:17 +0800

    add permissions

--- src/js/background.js
+++ src/js/background.js
@@ -1,2 +1,10 @@
 import '../img/icon-128.png'
 import '../img/icon-34.png'
+
+chrome.tabs.query({active:true},arr_tabs=>{
+    arr_tabs.forEach(tab=>{
+        console.log('------------------------------------');
+        console.log(tab.url);
+        console.log('------------------------------------');
+    });
+});
